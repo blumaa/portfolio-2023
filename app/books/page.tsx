bb0ec1e540b472d25fa2227c3825901f52bd2165
Aaron Blum <blumaa@gmail.com>
2023-12-08 17:20:59 +0100

    fix spelling

--- app/books/page.tsx
+++ app/books/page.tsx
@@ -5,7 +5,7 @@ export default function Books() {
   return (
     <div className="pt-4 ">
       <div className="flex flex-col items-center justify-center pb-4">
-        Here are some books and readings that I have really enjoye.
+        Here are some books and readings that I have really enjoyed.
       </div>
       <div className="flex flex-col items-center justify-center space-y-2">
         {books.map((book) => (
